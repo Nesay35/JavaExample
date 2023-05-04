6d2d9c8e5a282ae83837884c3182766a15465e8a
Nesrin Demir <nesrindemir35@gmail.com>
2023-05-04 23:34:04 +0300

    Seven commit

---
+++ ForLoop_TersUcgenSayi.java
@@ -0,0 +1,33 @@
+package java_github_practice;
+
+public class ForLoop_TersUcgenSayi {
+
+    /*
+       Şekli Yazdırınız:
+
+         1 2 3 4 5 6
+          2 3 4 5 6
+           3 4 5 6
+            4 5 6
+             5 6
+              6
+   */
+    public static void main(String[] args) {
+
+        for (int i=1; i<=6; i++){//Satır kontrolü
+
+            for(int bosluk=1; bosluk<i; bosluk++ ){
+                System.out.print(" ");
+            }
+
+            for(int j=i; j<=6; j++){//Sütun kontrolü
+
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+
+        }
+    }
+}
+
---
+++ List_OrtalamaUstuNotlar.java
@@ -0,0 +1,47 @@
+package java_github_practice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class List_OrtalamaUstuNotlar {
+
+    // Bir öğretmenden girmek istediği kadar notu alınız, ve ortalamayı geçen öğrenci sayısını bulunuz.
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        List<Double> notlar = new ArrayList<>();
+        String karar;
+        int sayac = 0;
+
+        //Kullanıcıdan istediği kadar not girmesini iste
+        do {
+            sayac++;
+            System.out.println(sayac + ". notu giriniz");
+            notlar.add(input.nextDouble());
+            System.out.println("Çıkmak için H, yeniden not girmek için herhangi başka bir karakter giriniz");
+            karar = input.next();
+
+        } while (!karar.equalsIgnoreCase("H"));
+        System.out.println("notlar = " + notlar);
+
+        //Ortalamayı hesapla
+        double toplam = 0;
+        for (double w : notlar) {
+            toplam += w;
+        }
+
+        double ortalama = toplam / notlar.size();
+        System.out.println("ortalama = " + ortalama);
+
+        //Ortalama üstü not sayısını bul
+        int ortalamaUstuNotSayisi = 0;
+        for (double w : notlar) {
+            if (w > ortalama) {
+                ortalamaUstuNotSayisi++;
+            }
+        }
+        System.out.println("ortalamaUstuNotSayisi = " + ortalamaUstuNotSayisi);
+
+    }
+
+}
